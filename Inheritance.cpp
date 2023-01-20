ba83aca1fefc8d28b517f6f8fb4e9697dd4bf472
Wasif Nawaz <nwasi1930@gmail.com>
2023-01-20 22:18:18 +0500

    net file Inheritance

---
+++ Inheritance.cpp
@@ -0,0 +1,21 @@
+//Inheritance
+#include iostream
+
+using namespace std;
+
+class Mobile{
+	private:
+		int IMEI;
+		string Modle;
+	public:
+		string color;
+		int numberOfCam;
+		bool is5G;
+};
+
+class OPPO : Mobile{
+	
+};
+int main(){
+	
+}
